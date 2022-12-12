537783fa9639f0a142f7ab1af70a02d7b15c8be6
Fabian Schulte-Brinker <1058080@bkrnet.de>
2022-12-12 16:22:09 +0100

    Basic program

---
+++ Lottery/Lottery/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace Lottery
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxBet = new System.Windows.Forms.TextBox();
+            this.buttonGamble = new System.Windows.Forms.Button();
+            this.labelBet = new System.Windows.Forms.Label();
+            this.labelBallance = new System.Windows.Forms.Label();
+            this.textBoxBalance = new System.Windows.Forms.TextBox();
+            this.labelWon = new System.Windows.Forms.Label();
+            this.textBoxWon = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 192);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 56);
+            this.label1.TabIndex = 0;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 260);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(56, 56);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(74, 260);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(56, 56);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(74, 192);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 56);
+            this.label2.TabIndex = 7;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(136, 260);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(56, 56);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(136, 192);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 56);
+            this.label3.TabIndex = 9;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(198, 260);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(56, 56);
+            this.textBox4.TabIndex = 12;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(198, 192);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 56);
+            this.label4.TabIndex = 11;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(260, 260);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(56, 56);
+            this.textBox5.TabIndex = 14;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(260, 192);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 56);
+            this.label5.TabIndex = 13;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(322, 260);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(56, 56);
+            this.textBox6.TabIndex = 16;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(322, 192);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 56);
+            this.label6.TabIndex = 15;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // textBoxBet
+            // 
+            this.textBoxBet.Location = new System.Drawing.Point(12, 41);
+            this.textBoxBet.Multiline = true;
+            this.textBoxBet.Name = "textBoxBet";
+            this.textBoxBet.Size = new System.Drawing.Size(94, 56);
+            this.textBoxBet.TabIndex = 17;
+            // 
+            // buttonGamble
+            // 
+            this.buttonGamble.Location = new System.Drawing.Point(16, 120);
+            this.buttonGamble.Name = "buttonGamble";
+            this.buttonGamble.Size = new System.Drawing.Size(362, 56);
+            this.buttonGamble.TabIndex = 18;
+            this.buttonGamble.Text = "GAMBLE";
+            this.buttonGamble.UseVisualStyleBackColor = true;
+            this.buttonGamble.Click += new System.EventHandler(this.buttonGamble_Click);
+            // 
+            // labelBet
+            // 
+            this.labelBet.Location = new System.Drawing.Point(12, 14);
+            this.labelBet.Name = "labelBet";
+            this.labelBet.Size = new System.Drawing.Size(94, 24);
+            this.labelBet.TabIndex = 19;
+            this.labelBet.Text = "Wettbetrag";
+            // 
+            // labelBallance
+            // 
+            this.labelBallance.Location = new System.Drawing.Point(284, 14);
+            this.labelBallance.Name = "labelBallance";
+            this.labelBallance.Size = new System.Drawing.Size(94, 24);
+            this.labelBallance.TabIndex = 23;
+            this.labelBallance.Text = "Ballance";
+            // 
+            // textBoxBalance
+            // 
+            this.textBoxBalance.Location = new System.Drawing.Point(284, 41);
+            this.textBoxBalance.Multiline = true;
+            this.textBoxBalance.Name = "textBoxBalance";
+            this.textBoxBalance.Size = new System.Drawing.Size(94, 56);
+            this.textBoxBalance.TabIndex = 22;
+            // 
+            // labelWon
+            // 
+            this.labelWon.Location = new System.Drawing.Point(149, 14);
+            this.labelWon.Name = "labelWon";
+            this.labelWon.Size = new System.Drawing.Size(94, 24);
+            this.labelWon.TabIndex = 25;
+            this.labelWon.Text = "Gewinn";
+            // 
+            // textBoxWon
+            // 
+            this.textBoxWon.Location = new System.Drawing.Point(149, 41);
+            this.textBoxWon.Multiline = true;
+            this.textBoxWon.Name = "textBoxWon";
+            this.textBoxWon.Size = new System.Drawing.Size(94, 56);
+            this.textBoxWon.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 333);
+            this.Controls.Add(this.labelWon);
+            this.Controls.Add(this.textBoxWon);
+            this.Controls.Add(this.labelBallance);
+            this.Controls.Add(this.textBoxBalance);
+            this.Controls.Add(this.labelBet);
+            this.Controls.Add(this.buttonGamble);
+            this.Controls.Add(this.textBoxBet);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxBet;
+        private System.Windows.Forms.Button buttonGamble;
+        private System.Windows.Forms.Label labelBet;
+        private System.Windows.Forms.Label labelBallance;
+        private System.Windows.Forms.TextBox textBoxBalance;
+        private System.Windows.Forms.Label labelWon;
+        private System.Windows.Forms.TextBox textBoxWon;
+    }
+}
+
---
+++ Lottery/Lottery/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lottery
+{
+    public partial class Form1 : Form
+    {
+        Random rndm = new Random();
+        int[] rnd = new int[6];
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGamble_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                for (int f = 0; f < 6; f++)
+                {
+                    rnd[i] = rndm.Next(0, 100);
+                    if (rnd[i] == rnd[f]) i -= 1;
+                }
+            }
+
+            label1.Text = Convert.ToString(rnd[0]);
+            label2.Text = Convert.ToString(rnd[1]);
+            label3.Text = Convert.ToString(rnd[2]);
+            label4.Text = Convert.ToString(rnd[3]);
+            label5.Text = Convert.ToString(rnd[4]);
+            label6.Text = Convert.ToString(rnd[5]);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
